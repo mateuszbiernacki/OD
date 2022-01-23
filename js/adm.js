3ddaffa8588b2cf77e888116bd92f4ae9f0c70a1
mateuszbiernacki <44376347+mateuszbiernacki@users.noreply.github.com>
2022-01-23 18:11:42 +0100

    admin panel

--- js/adm.js
+++ js/adm.js
@@ -11,11 +11,31 @@ function get_groups() {
         url: 'http://127.0.0.1:5000/groups',
         dataType: 'json',
         contentType: 'application/json',
+        type: 'GET',
         traditional: true,
         success: function (response) {
             $("#ul-groups").append()
             $.each(response, function (index, value){
                 $("#ul-groups").append("<li>"+ value+ "</li>")
+            })
+        },
+        error: function (response) {
+            console.log(response)
+        }
+    })
+}
+
+function get_votings() {
+    $.ajax({
+        url: 'http://127.0.0.1:5000/allVotings',
+        dataType: 'json',
+        contentType: 'application/json',
+        type: 'GET',
+        traditional: true,
+        success: function (response) {
+            $("#ul-votings").append()
+            $.each(response, function (index, value){
+                $("#ul-votings").append("<li>"+ value+ "</li>")
             })
         },
         error: function (response) {
